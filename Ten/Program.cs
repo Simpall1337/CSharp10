a468a16b679b9fbaf51a79901d248bdf024bedaf
Alex Huzo <sanduguzo1337@gmail.com>
2022-09-19 13:20:04 +0300

    Ten

---
+++ Ten/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Lessons
+{
+    internal class Program
+    {
+        static void outputArray(ref int[] ArrayConsole, int i)
+        {
+            Console.WriteLine(ArrayConsole[ArrayConsole.Length - i]);
+            if (i == ArrayConsole.Length)
+                return;
+            i++;
+            outputArray(ref ArrayConsole, i);
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[5] { 1, 2, 3, 4, 5 };
+            outputArray(ref myArray, 1);
+        }
+    }
+}
